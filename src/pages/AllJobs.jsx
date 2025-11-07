8953f6ba7631abea6a72e16386d6991e7540dee4
sumon <sms.sumon@gmail.com>
2025-11-08 05:20:30 +0600

    updated rest button

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -7,26 +7,31 @@ const AllJobs = () => {
   const [currentpage, setCurrentPage] = useState(1);
   const [count, setCount] = useState(0);
   const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState();
+  const [search, setSearch] = useState("");
+  const [searchText, setSearchText] = useState("");
   const [jobs, setJobs] = useState([]);
   const axiosSecure = useAxiosSecure();
   useEffect(() => {
     const getData = async () => {
       const { data } = await axiosSecure(
-        `/all-jobs?page=${currentpage}&size=${itemsPerPage}&filter=${filter}`
+        `/all-jobs?page=${currentpage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`
       );
       setJobs(data);
     };
     getData();
-  }, [currentpage, itemsPerPage, filter]);
+  }, [currentpage, itemsPerPage, filter, sort, search]);
 
   useEffect(() => {
     const getCount = async () => {
-      const { data } = await axiosSecure(`/jobs-count?filter=${filter}`);
+      const { data } = await axiosSecure(
+        `/jobs-count?filter=${filter}&search=${search}`
+      );
 
       setCount(data.count);
     };
     getCount();
-  }, [filter]);
+  }, [filter, search]);
   console.log(count);
   // const pages = [1, 2, 3, 4];
   const numberOfPages = Math.ceil(count / itemsPerPage);
@@ -37,6 +42,18 @@ const AllJobs = () => {
     console.log(value);
     setCurrentPage(value);
   };
+  const handleReset = () => {
+    setFilter("");
+    setSort("");
+    setSearch("");
+    setSearchText("");
+  };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // const text = e.target.search.value;
+    setSearch(searchText);
+  };
+  console.log(search);
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
       <div>
@@ -59,11 +76,13 @@ const AllJobs = () => {
             </select>
           </div>
 
-          <form>
+          <form onSubmit={handleSearch}>
             <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
               <input
                 className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
                 type="text"
+                onChange={(e) => setSearchText(e.target.value)}
+                value={searchText}
                 name="search"
                 placeholder="Enter Job Title"
                 aria-label="Enter Job Title"
@@ -76,8 +95,13 @@ const AllJobs = () => {
           </form>
           <div>
             <select
-              name="category"
-              id="category"
+              onChange={(e) => {
+                setSort(e.target.value);
+                setCurrentPage(1);
+              }}
+              value={sort}
+              name="sort"
+              id="sort"
               className="border p-4 rounded-md"
             >
               <option value="">Sort By Deadline</option>
@@ -85,7 +109,9 @@ const AllJobs = () => {
               <option value="asc">Ascending Order</option>
             </select>
           </div>
-          <button className="btn">Reset</button>
+          <button onClick={handleReset} className="btn">
+            Reset
+          </button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {jobs.map((job) => (
@@ -127,7 +153,7 @@ const AllJobs = () => {
             key={btnNum}
             className={`hidden ${
               currentpage === btnNum ? "bg-blue-500 text-white" : ""
-            }px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            } px-4 py-2 mx-1 transition-colors duration-300 transform rounded-md sm:inline hover:bg-blue-500 hover:text-white`}
           >
             {btnNum}
           </button>
